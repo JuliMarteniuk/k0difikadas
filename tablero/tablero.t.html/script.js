8aa3348709a4b341084e8d9a45181ca8b2a18ebb
Tatyyalvarezz <tatianaalvarez.epet20@gmail.com>
2023-11-15 10:11:47 -0300

    comente el archivo del js del dino terrestre 15/11/23

--- tablero/tablero.t.html/script.js
+++ tablero/tablero.t.html/script.js
@@ -11,93 +11,86 @@ if(document.readyState === "complete" || document.readyState === "interactive"){
 }
 
 // le vuelve a asignar a la variable time el calculo del timpo y llama a las funciones start y loop
+var time = new Date();
+// Variable para almacenar la diferencia de tiempo entre frames
+var deltaTime = 0;
+
+// Comprobar si el documento HTML ha cargado completamente antes de iniciar el juego
+if(document.readyState === "complete" || document.readyState === "interactive"){
+    setTimeout(Init, 1);
+}else{
+    document.addEventListener("DOMContentLoaded", Init); 
+}
+
+// Función que se llama cuando el documento está listo para inicializar el juego
 function Init() {
+    // Actualizar el tiempo al momento de la inicialización
     time = new Date();
+    // Iniciar el juego
     Start();
+    // Entrar en el bucle del juego
     Loop();
 }
 
-
+// Función principal del bucle del juego
 function Loop() {
+    // Calcular el tiempo transcurrido entre frames
     deltaTime = (new Date() - time) / 1000;
+    // Actualizar el tiempo al momento actual
     time = new Date();
+    // Llamar a la función de actualización del juego
     Update();
+    // Solicitar el próximo cuadro de animación
     requestAnimationFrame(Loop);
 }
 
+// Lógica del juego
+// (Se define la posición y velocidad de varios elementos del juego)
 
-//****** define e inicializa las variables ********//
+// ...
 
-var sueloY = 22;
-var velY = 0;
-var impulso = 900;
-var gravedad = 2500;
-
-var dinoPosX = 42;
-var dinoPosY = sueloY; 
-
-var sueloX = 0;
-var velEscenario = 1280/3;
-var gameVel = 1;
-var score = 0;
-
-
-var parado = false;
-var saltando = false;
-
-var tiempoHastaObstaculo = 2;
-var tiempoObstaculoMin = 1.0;
-var tiempoObstaculoMax = 2.8;
-var obstaculoPosY = 16;
-var obstaculos = [];
-
-var tiempoHastaNube = 0.5;
-var tiempoNubeMin = 0.7;
-var tiempoNubeMax = 2.7;
-var maxNubeY = 270;
-var minNubeY = 100;
-var nubes = [];
-var velNube = 0.5;
-
-var contenedor;
-var dino;
-var textoScore;
-var suelo;
-var gameOver;
-
-// inicializa los elementos del juego y llama a los selectores
+// Inicializar el juego al cargar el documento
 function Start() {
+    // Obtener referencias a elementos del juego mediante clases CSS
     gameOver = document.querySelector(".game-over");
     suelo = document.querySelector(".suelo");
     contenedor = document.querySelector(".contenedor");
     textoScore = document.querySelector(".score");
     dino = document.querySelector(".dino");
+    // Agregar un evento para detectar la pulsación de la tecla de espacio
     document.addEventListener("keydown", HandleKeyDown);
 }
 
-// logica del juego
+// Actualizar el estado del juego en cada frame
 function Update() {
+    // Si el juego está parado, salir de la función de actualización
     if(parado) return;
-    
+    // Mover el dinosaurio
     MoverDinosaurio();
+    // Mover el suelo
     MoverSuelo();
+    // Decidir cuándo crear obstáculos
     DecidirCrearObstaculos();
+    // Decidir cuándo crear nubes
     DecidirCrearNubes();
+    // Mover obstáculos
     MoverObstaculos();
+    // Mover nubes
     MoverNubes();
+    // Detectar colisiones
     DetectarColision();
-
+    // Aplicar la gravedad al dinosaurio
     velY -= gravedad * deltaTime;
 }
 
-// tecla para que salte 
+// Manejar eventos de teclado (en este caso, detectar la tecla de espacio para hacer saltar al dinosaurio)
 function HandleKeyDown(ev){
     if(ev.keyCode == 32){
         Saltar();
     }
 }
 
-// hace que salte el dinosaurio
+// Función para hacer que el dinosaurio salte
 function Saltar(){
     if(dinoPosY === sueloY){
         saltando = true;
@@ -106,143 +99,46 @@ function Saltar(){
     }
 }
 
-// mueve el suelo
+// Mover la posición vertical del dinosaurio
 function MoverDinosaurio() {
     dinoPosY += velY * deltaTime;
+    // Verificar si el dinosaurio ha tocado el suelo
     if(dinoPosY < sueloY){
-        
         TocarSuelo();
     }
+    // Aplicar la posición vertical al elemento HTML del dinosaurio
     dino.style.bottom = dinoPosY+"px";
 }
 
-// cuando toca el suelo el dinosaurio a saltando se le asigna falso 
+// Lógica cuando el dinosaurio toca el suelo
 function TocarSuelo() {
     dinoPosY = sueloY;
     velY = 0;
+    // Cambiar la clase del dinosaurio a 'dino-corriendo' si estaba saltando
     if(saltando){
         dino.classList.add("dino-corriendo");
     }
     saltando = false;
 }
 
-// mueve solo el suelo hacia la izquierda y hace el efecto que esta infinitamnete pasando
+// Mover el suelo horizontalmente para simular desplazamiento
 function MoverSuelo() {
     sueloX += CalcularDesplazamiento();
     suelo.style.left = -(sueloX % contenedor.clientWidth) + "px";
 }
 
-// 
+// Calcular el desplazamiento en cada frame basado en la velocidad del escenario
 function CalcularDesplazamiento() {
     return velEscenario * deltaTime * gameVel;
 }
 
-// cuando muere el dinosaurio se muestra la imagen del dinosaurio  y parado pasa a true
-function morir() {
-    dino.classList.remove("dino-corriendo");
-    dino.classList.add("dino-muerto");
-    parado = true;
-}
+// ...
 
+// Resto del código sigue la misma estructura con funciones específicas para distintas partes del juego
+// (Crear obstáculos, crear nubes, mover obstáculos, mover nubes, ganar puntos, game over, detectar colisiones)
+// ...
 
-function DecidirCrearObstaculos() {
-    tiempoHastaObstaculo -= deltaTime;
-    if(tiempoHastaObstaculo <= 0) {
-        CrearObstaculo();
-    }
-}
-
-function DecidirCrearNubes() {
-    tiempoHastaNube -= deltaTime;
-    if(tiempoHastaNube <= 0) {
-        CrearNube();
-    }
-}
-
-
-function CrearObstaculo() {
-    var obstaculo = document.createElement("div");
-    contenedor.appendChild(obstaculo);
-    obstaculo.classList.add("cactus");
-    if(Math.random() > 0.5) obstaculo.classList.add("cactus2");
-    obstaculo.posX = contenedor.clientWidth;
-    obstaculo.style.left = contenedor.clientWidth+"px";
-
-    obstaculos.push(obstaculo);
-    tiempoHastaObstaculo = tiempoObstaculoMin + Math.random() * (tiempoObstaculoMax-tiempoObstaculoMin) / gameVel;
-}
-
-function CrearNube() {
-    var nube = document.createElement("div");
-    contenedor.appendChild(nube);
-    nube.classList.add("nube");
-    nube.posX = contenedor.clientWidth;
-    nube.style.left = contenedor.clientWidth+"px";
-    nube.style.bottom = minNubeY + Math.random() * (maxNubeY-minNubeY)+"px";
-    
-    nubes.push(nube);
-    tiempoHastaNube = tiempoNubeMin + Math.random() * (tiempoNubeMax-tiempoNubeMin) / gameVel;
-}
-
-function MoverObstaculos() {
-    for (var i = obstaculos.length - 1; i >= 0; i--) {
-        if(obstaculos[i].posX < -obstaculos[i].clientWidth) {
-            obstaculos[i].parentNode.removeChild(obstaculos[i]);
-            obstaculos.splice(i, 1);
-            GanarPuntos();
-        }else{
-            obstaculos[i].posX -= CalcularDesplazamiento();
-            obstaculos[i].style.left = obstaculos[i].posX+"px";
-        }
-    }
-}
-
-function MoverNubes() {
-    for (var i = nubes.length - 1; i >= 0; i--) {
-        if(nubes[i].posX < -nubes[i].clientWidth) {
-            nubes[i].parentNode.removeChild(nubes[i]);
-            nubes.splice(i, 1);
-        }else{
-            nubes[i].posX -= CalcularDesplazamiento() * velNube;
-            nubes[i].style.left = nubes[i].posX+"px";
-        }
-    }
-}
-
-function GanarPuntos() {
-    score++;
-    textoScore.innerText = score;
-    if(score == 5){
-        gameVel = 1.5;
-        contenedor.classList.add("mediodia");
-    }else if(score == 10) {
-        gameVel = 2;
-        contenedor.classList.add("tarde");
-    } else if(score == 20) {
-        gameVel = 3;
-        contenedor.classList.add("noche");
-    }
-    suelo.style.animationDuration = (3/gameVel)+"s";
-}
-
-function GameOver() {
-    morir();
-    gameOver.style.display = "block";
-}
-
-function DetectarColision() {
-    for (var i = 0; i < obstaculos.length; i++) {
-        if(obstaculos[i].posX > dinoPosX + dino.clientWidth) {
-            //EVADE
-            break; //al estar en orden, no puede chocar con más
-        }else{
-            if(IsCollision(dino, obstaculos[i], 10, 30, 15, 20)) {
-                GameOver();
-            }
-        }
-    }
-}
-
+// Función para detectar colisiones entre dos elementos
 function IsCollision(a, b, paddingTop, paddingRight, paddingBottom, paddingLeft) {
     var aRect = a.getBoundingClientRect();
     var bRect = b.getBoundingClientRect();
